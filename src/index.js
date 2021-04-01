3300db1010db85282b9db3c278bbdebc05f98ed6
KAROL\Karol <karol.moleda90@gmail.com>
2021-04-01 14:24:27 +0200

    samolot

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import style from "./index.scss"
 
+
 import {currency} from "./currency"
 import {clock} from "./clock"
 import {dateout} from "./date"
 import {choice} from "./point"
+// import {plane} from "./plane"
+document.getElementById("submit").addEventListener('click',plane)
+
+function plane(){
+    if(document.getElementById("ple").classList.contains("1")){
+        document.getSelection("main").classList.add("disappear")
+    }
+
+    }
 
 
 
---
+++ src/plane.js
@@ -0,0 +1,12 @@
+document.querySelector("submit").addEventListener('click',plane)
+
+function plane(){
+    if(document.querySelector("ple").classList.contains("1")){
+        document.querySelector("main").classList.add("disappear")
+    }
+
+    }
+
+
+
+// export const plane = function () {plane}
