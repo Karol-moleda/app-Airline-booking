82bdf88d2da135b307a13bb5cba4e247c4fcd0c2
KAROL\Karol <karol.moleda90@gmail.com>
2021-04-22 22:48:56 +0200

    samoloty+ustawienie button

--- src/index.js
+++ src/index.js
@@ -1,19 +1,41 @@
 import style from "./index.scss"
+import {currency} from "./currency";
+import {clock} from "./clock";
+import {dateout} from "./date";
 
 
-import {currency} from "./currency"
-import {clock} from "./clock"
-import {dateout} from "./date"
-import {choice} from "./point"
-// import {plane} from "./plane"
-document.getElementById("submit").addEventListener('click',plane)
 
-function plane(){
-    if(document.getElementById("ple").classList.contains("1")){
-        document.getSelection("main").classList.add("disappear")
+console.log(document.getElementById("1").value)
+document.getElementById('Wybierz').addEventListener("click",Fly);
+
+function Fly(){
+    if (document.getElementById("1").value===1){
+        // document.getElementsByClassName("Linie").classList.add("disappear");
+        document.getElementsByClassName("place").classList.add("disappear");
+        document.getElementsByClassName("other").classList.add("disappear");
+        document.getElementById("lot").classList.add("disappear");
+        document.getElementById("poradnik").classList.add("disappear");
+        document.getElementById("planeAtlanta").classList.add("active");
+    } 
+    else if(document.getElementById("2").value===2) {
+        // document.getElementsByClassName("Linie").classList.add("disappear");
+        document.getElementsByClassName("place").classList.add("disappear");
+        document.getElementsByClassName("other").classList.add("disappear");
+        document.getElementById("lot").classList.add("disappear");
+        document.getElementById("poradnik").classList.add("disappear");
+        document.getElementById("planePekin").classList.add("active");
     }
-
+    else{
+        // document.getElementsByClassName("Linie").classList.add("disappear");
+        document.getElementsByClassName("place").classList.add("disappear");
+        document.getElementsByClassName("other").classList.add("disappear");
+        document.getElementById("lot").classList.add("disappear");
+        document.getElementById("poradnik").classList.add("disappear");
+        document.getElementById("planeLondyn").classList.add("active")
     }
+};
+
+
 
 
 
